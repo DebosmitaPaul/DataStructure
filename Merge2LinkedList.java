ff8156252a6424dee70dea97b0cd8b51da2c305b
DebosmitaPaul <debo.paul03@gmail.com>
2021-04-15 21:39:56 +0530

    Merger two linked list in 2 way[1.Creating new reasult linked list.  2.Using exsisting left/right linked list.]

--- Merge2LinkedList.java
+++ Merge2LinkedList.java
@@ -10,24 +10,18 @@ public class Merge2LinkedList {
     public static void main(String[] args) {
         Merge2LinkedList list = new Merge2LinkedList();
         Node leftNode = list.createLinkedList1();
+        Node rightNode = list.createLinkedList2();
+
         System.out.println("Left node..");
         list.displayLinedlist(leftNode);
        // Node head = list.InsertNode(100,leftNode);
-        Node rightNode = list.createLinkedList2();
         System.out.println("Right node..");
         list.displayLinedlist(rightNode);
 
-        Node resNode =  list.mergeList(leftNode,rightNode);
+        Node resNode = list.mergeList_Method2(leftNode,rightNode);
+                      //list.mergeList(leftNode,rightNode);
         System.out.println("Result node..");
         list.displayLinedlist(resNode);
-    }
-
-    private void displayLinedlist(Node node) {
-        while (node!= null){
-            System.out.print(node.data+" ");
-            node = node.next;
-        }
-        System.out.println();
     }
 
     private Node mergeList(Node leftNode, Node rightNode) {
@@ -42,11 +36,11 @@ public class Merge2LinkedList {
                 rightNode = rightNode.next;
             }
         }
-        while (leftNode!= null){
+        if (leftNode!= null){
             result = insertNode(leftNode.data, result);
             leftNode = leftNode.next;
         }
-        while (rightNode!=null){
+        else if (rightNode!=null){
             result = insertNode(rightNode.data,result);
             rightNode = rightNode.next;
         }
@@ -66,6 +60,80 @@ public class Merge2LinkedList {
         return head;
     }
 
+    private Node mergeList_Method2(Node leftNode, Node rightNode){
+        Node result = null;
+        if(leftNode.data<rightNode.data){
+            result = leftNode;
+            leftNode= leftNode.next;
+            result = method_Left(result,leftNode,rightNode);
+        }
+        else{
+            result = rightNode;
+            rightNode = rightNode.next;
+            result = method_Right(result,leftNode,rightNode);
+        }
+        return result;
+    }
+    private Node method_Right(Node result, Node leftNode, Node rightNode){
+        while (leftNode != null && rightNode != null) {
+            if (leftNode.data < rightNode.data) {
+                Node node = leftNode;
+                leftNode = leftNode.next;
+                node.next = null;
+                result = insertNodeInBetween(node, result);
+            }
+            else {
+                rightNode = rightNode.next;
+            }
+        }
+        if (leftNode!=null){
+            result = insertAtLast(leftNode, result);
+        }
+        return result;
+    }
+    private Node method_Left(Node result, Node leftNode, Node rightNode){
+        while (leftNode != null && rightNode != null) {
+            if (rightNode.data < leftNode.data) {
+                Node node = rightNode;
+                rightNode = rightNode.next;
+                node.next = null;
+                result = insertNodeInBetween(node, result);
+            }
+            else {
+                leftNode = leftNode.next;
+            }
+        }
+        if (rightNode!=null){
+            result = insertAtLast(rightNode, result);
+        }
+        return result;
+    }
+    private Node insertNodeInBetween(Node insertedNode, Node head){
+        Node current = head.next;
+        Node prev = head;
+        while(current!=null && current.data<insertedNode.data){
+            current = current.next;
+            prev = prev.next;
+        }
+        prev.next = insertedNode;
+        insertedNode.next = current;
+        return head;
+    }
+    private Node insertAtLast(Node insertedNode,Node result){
+        Node current = result;
+        while (current.next != null){
+            current = current.next;
+        }
+        current.next = insertedNode;
+        return result;
+    }
+    private void displayLinedlist(Node node) {
+        while (node!= null){
+            System.out.print(node.data+" ");
+            node = node.next;
+        }
+        System.out.println();
+    }
     private Node createLinkedList1() {
         Node head = new Node(2);
         head.next = new Node(5);
@@ -75,11 +143,11 @@ public class Merge2LinkedList {
         return head;
     }
     private Node createLinkedList2() {
-        Node head = new Node(1);
+        Node head = new Node(3);
         head.next = new Node(7);
         head.next.next= new Node(9);
         head.next.next.next = new Node(15);
-        head.next.next.next.next= new Node(25);
+        //head.next.next.next.next= new Node(25);
         return head;
     }
 }
